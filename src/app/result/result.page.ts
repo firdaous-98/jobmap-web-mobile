32746f5a62f2decb70b9975bfe5d9172c4054b83
Anass LAMAALAM <anasslam69@gmail.com>
2022-02-01 01:37:02 +0100

    create result component

--- src/app/result/result.page.ts
+++ src/app/result/result.page.ts
@@ -7,9 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResultPage implements OnInit {
 
+  RHeight: string = '';
+  IHeight: string = '';
+  AHeight: string = '';
+
+  array: { key: string, value: number }[] = [
+    { 'key': 'R', 'value': 10 },
+    { 'key': 'I', 'value': 15 },
+    { 'key': 'A', 'value': 7 }
+  ];
   constructor() { }
 
   ngOnInit() {
+    this.calculChartHeight();
+  }
+
+  calculChartHeight() {
+    const total = this.array.reduce((a, b) => a + b.value, 0);
+    this.RHeight = (this.array.filter(item => item.key === 'R')[0].value * 170 / total).toString() + 'px';
+    this.IHeight = (this.array.filter(item => item.key === 'I')[0].value * 170 / total).toString() + 'px';
+    this.AHeight = (this.array.filter(item => item.key === 'A')[0].value * 170 / total).toString() + 'px';
   }
 
 }
